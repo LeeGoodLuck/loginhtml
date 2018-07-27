af7ecc1592e9854cc93e310b200d243dc70fc0a6
LeeGoodLuck <464858263@qq.com>
2018-07-27 18:10:09 +0800

    登录页面

---
+++ js/common.js
@@ -0,0 +1,25 @@
+(function(){
+	var common = {
+		url: function(){
+			return 'http://192.168.0.125:16655'
+		},
+		ajax: function(url, type, data, callback){
+			$.ajax({
+				url: common.url() + url,
+				type: type,
+				data: data,
+				success: function(result){
+					callback(result)
+				},
+				error: function(reject){
+					try{
+						console.log(reject);
+					}catch{
+						console.log('500');
+					}
+				}
+			})
+		}
+	}
+	window.common = common
+})();
